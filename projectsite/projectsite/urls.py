4abf7d8cc8ace9236bdf1a604bbe0200abc829a0
theyvebeensentel <202180153@psu.palawan.edu.ph>
2026-09-10 21:55:56 +0800

    changed SITE_ID

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from studentorg import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.HomePageView.as_view(), name='home'),
